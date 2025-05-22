5f8c4854315cf68b07ae0ae9cad509c528494e84
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2025-05-22 10:42:00 +0100

    Boucles

---
+++ boucles/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Coucou tout le monde !</h1>
+
+    <?php
+    $maVariable = 1;
+    $pokemons = ["Dracaufeu", "Lucario", "Lugia"];
+
+    $pokemons[$maVariable] = "bidule";
+
+    $pokemons[] = "Darkrai";
+
+    var_dump($pokemons);
+
+    $i = 0;
+
+    while ($i <= 10)
+    {
+        echo "<p>$i</p>";
+        $i = $i + 1; // Strictement equivalent à "$i++;"
+    }
+
+    foreach ($pokemons as $pokemon)
+    {
+        echo "<p>$pokemon</p>";
+    }
+
+    ?>
+
+</body>
+</html>
