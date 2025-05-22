4de45db3c72647a3d1a429424ce82f4d677fc55e
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2025-05-22 13:36:42 +0100

    Ajout header

---
+++ json/header.php
@@ -0,0 +1,16 @@
+<header>
+    <h1>Wsh ma gl !!!!</h1>
+
+    <?php 
+    
+        if (isset($nicolasDutertre))
+        {
+            echo $nicolasDutertre; 
+        }
+        else
+        {
+            echo "PTDR C KI ?";
+        }
+
+    ?>
+</header>
