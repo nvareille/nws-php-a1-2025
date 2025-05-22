60dd104cc8837612be178e11b364c37bed478ad3
Nicolas VAREILLE <nicolasvareille.gdl@gmail.com>
2025-05-22 09:04:56 +0100

    repo

---
+++ variable/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Coucou tout le monde !</h1>
+
+    <?php
+
+    $maVariable = 30;
+    $nomAnimal = "Mina";
+
+    echo "Je m'appelle Nicolas et j'ai $maVariable et mon chat s'appelle $nomAnimal";
+
+    ?>
+
+</body>
+</html>
